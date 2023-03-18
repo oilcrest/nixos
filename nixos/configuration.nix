bd13c072f6826bd311a520a621926f85f37c50f6
John Garcia <jgarcia3788@yahoo.co.uk>
2023-03-18 21:09:41 +0000

    Merged my nix config

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -2,16 +2,18 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 let
   impermanence = builtins.fetchTarball "https://github.com/nix-community/impermanence/archive/master.tar.gz";
+  packageGroups = import ./packages.nix { inherit pkgs; };
 in
 {
   imports =
     [
       "${impermanence}/nixos.nix"
       ./hardware-configuration.nix
+      ./users.nix
     ];
 
   # filesystems
@@ -19,9 +21,9 @@ in
   fileSystems."/home".options = ["compress=zstd" "noatime" ];
   fileSystems."/nix".options = ["compress=zstd" "noatime" ];
   fileSystems."/persist".options = ["compress=zstd" "noatime" ];
-  fileSystems."/persist".neededForBoot = true;
-  
   fileSystems."/var/log".options = ["compress=zstd" "noatime" ];
+
+  fileSystems."/persist".neededForBoot = true;
   fileSystems."/var/log".neededForBoot = true;
 
   # reset / at each boot
@@ -72,11 +74,49 @@ in
     ];
     files = [
       "/etc/machine-id"
-      "/etc/ssh/ssh_host_ed25519_key"
-      "/etc/ssh/ssh_host_ed25519_key.pub"
-      "/etc/ssh/ssh_host_rsa_key"
-      "/etc/ssh/ssh_host_rsa_key.pub"
+      #"/etc/ssh/ssh_host_ed25519_key"
+      #"/etc/ssh/ssh_host_ed25519_key.pub"
+      #"/etc/ssh/ssh_host_rsa_key"
+      #"/etc/ssh/ssh_host_rsa_key.pub"
     ];
+  };
+
+  nixpkgs.config = {
+    allowUnfree = true;
+    packageOverrides = pkgs: {
+      unstable = import <nixos-unstable> {
+        config = config.nixpkgs.config;
+      };
+    };
+  };
+
+  systemd.user.services.spice-agent = {
+    enable = true;
+    wantedBy = ["graphical-session.target"]; 
+    serviceConfig = { ExecStart = "${pkgs.spice-vdagent}/bin/spice-vdagent -x"; }; 
+    unitConfig = { ConditionVirtualization = "vm"; 
+      Description = "Spice guest session agent";
+      After = ["graphical-session-pre.target"];
+      PartOf = ["graphical-session.target"]; 
+    }; 
+  }; 
+
+
+  # https://nixos.wiki/wiki/Storage_optimization
+  # nix.autoOptimiseStore = true;
+  nix.settings.auto-optimise-store = true;
+  # Garbage Collection 
+  nix.gc = {
+    automatic = true;
+    dates = "daily";
+    options = "--delete-older-than 3d";
+  };
+
+  nix = {
+    extraOptions = ''
+      experimental-features = nix-command
+      extra-experimental-features = flakes
+    '';
   };
 
   security.sudo.extraConfig = ''
@@ -84,46 +124,211 @@ in
     Defaults lecture = never
   '';
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  # Boot loader
+  boot.loader = {
+    efi.canTouchEfiVariables=true;
+    # systemd-boot.enable = true;
+    grub = { 
+        enable = true;
+        devices = [ "nodev" ];
+        efiSupport = true;
+    };
+    timeout = 3;
+  };
 
+
+
+  time.timeZone = "Europe/London";
+
+  # Internationalisation
+  i18n.defaultLocale = "en_GB.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "uk";
+  };
+
+
+  ################
+  ##  Network  ###
+  ################
   networking.hostName = "nixos";
   networking.networkmanager.enable = true;
 
-  time.timeZone = "Europe/Amsterdam";
-
-  users.mutableUsers = false;
-  users.users.user = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ];
-
-    openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICWVNch9BcjkMqS/Xwep+GN4HwqyRIjr3Cuw7mHpqsKr nixos" ];
-
-    # passwordFile needs to be in a volume marked with `neededForBoot = true`
-    passwordFile = "/persist/passwords/user";
-  };
-
-  services.openssh = {
-    enable = true;
-    passwordAuthentication = false;
-    allowSFTP = false; # Don't set this if you need sftp
-    challengeResponseAuthentication = false;
-    extraConfig = ''
-      AllowTcpForwarding yes
-      X11Forwarding no
-      AllowAgentForwarding no
-      AllowStreamLocalForwarding no
-      AuthenticationMethods publickey
-    '';
-  };
-
   # Open ports in the firewall.
-  networking.firewall = {
-    enable = true;
-    allowedTCPPorts = [ 22 ];
-    allowedUDPPorts = [ ];
+  # networking.firewall = {
+  #   enable = true;
+  #   allowedTCPPorts = [ 22 ];
+  #   allowedUDPPorts = [ ];
+  # };
+
+
+  #############
+  ## Desktop ##
+  #############
+
+  # Enable X11, sddm and KDE plasma.
+  services.xserver = {
+	enable=true;
+  	displayManager.sddm.enable = true;
+  	desktopManager.plasma5.enable = true;
+	#displayManager.setupCommands = "xrandr -display :0.0 --output Virtual-1 --mode 1920x1080";
+    # X11 keymap
+    layout = "gb";
   };
+
+
+  #############
+  ## Kernel ###
+  #############
+
+  # Custom Kernels
+  # boot.kernelPackages = pkgs.linuxPackages_zen;
+  # boot.kernelPackages = pkgs.linuxPackages_latest;
+  # boot.kernelPackages = pkgs.linuxPackages_hardened;
+
+
+  ################
+  ### Drivers ####
+  ################
+
+  ### OpenGL ###
+  # hardware.opengl = {
+  #   enable = true;
+  #   driSupport = true;
+  #   driSupport32Bit = true;
+  #   extraPackages = [ pkgs.libGL pkgs.libGLU ];
+  # };
+
+
+  ################
+  ### Security ###
+  ################
+
+  services.packagekit.enable = true;
+  services.fwupd.enable = true;
+
+  # Enable Apparmor
+  security.apparmor.enable = true;
+
+  # Enable doas. Turn off sudo.
+  security.doas.enable = true;
+  security.sudo.enable = true;
+
+  # Mount /tmp as tmpfs
+  boot.tmpOnTmpfs = true;
+  
+  # Clean-out /var/tmp for files older than 7 days. 
+  # systemd.tmpfiles.rules = [ "q /var/tmp 1777 root root 7d" ];
+  # systemd.tmpfiles.rules = [ "d /var/tmp 1777 root root 7d" ];
+
+
+
+  ################
+  ### Programs ###
+  ################
+
+  programs.neovim = {
+    enable = true;
+    # package = unstable.neovim;
+    viAlias = true;
+    vimAlias = true;
+  };
+
+  programs.zsh.enable = true;
+  # environment.shellAliases = {
+  #   vim = "nvim";
+  # };
+
+  # programs.chromium = {
+  #   enable = true;
+  #   homepageLocation = "https://nixos.org"; 
+  #   extensions =
+  #   [
+  #   "gcbommkclmclpchllfjekcdonpmejbdp" # https everywhere
+  #   "cjpalhdlnbpafiamejdnhcphjbkeiagm" # ublock origin
+  #   ];	
+  # };
+
+  
+  # System Packages to install
+  environment.systemPackages = with packageGroups; my-package-set;
+
+  # Install specific Nerd fonts 
+  fonts.fonts = with pkgs; [
+    (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" "Hack" ]; })
+  ];
+
+  # Firejailed apps
+  # programs.firejail = {
+  #   enable = true;
+  #   wrappedBinaries = 
+  #   {     
+  #     # librewolf
+  #     jailwolf = {
+  #       executable = "${lib.getBin pkgs.librewolf}/bin/librewolf";
+  #       profile = "${pkgs.firejail}/etc/firejail/librewolf.profile";
+  #       extraArgs = [ "--private=~/sandbox" ];
+  #     };
+  #     # firefox
+  #     jailfox = {
+  #       executable = "${lib.getBin pkgs.firefox}/bin/firefox --no-remote";
+  #       profile = "${pkgs.firejail}/etc/firejail/firefox.profile";
+  #       extraArgs = [ "--private=~/sandbox" ];
+  #     };
+  #   };
+  # };
+
+
+  ############
+  # Services #
+  ############
+ 
+  ### Sound ###
+  # https://nixos.wiki/wiki/PipeWire
+  # rtkit is optional but recommended
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  # Uncomment to use JACK applications:
+  # jack.enable = true;
+  };
+
+
+  ### Flatpak ###
+  services.flatpak.enable = true;
+  # Daily updates wit systemd
+  systemd.services.flatpak-update = {
+    serviceConfig.Type = "oneshot";
+    path = [ pkgs.flatpak ];
+    serviceConfig.ExecStart = "${pkgs.flatpak}/bin/flatpak update -y";
+  };
+  systemd.timers.flatpak-update = {
+    wantedBy = [ "timers.target" ];
+    partOf = [ "flatpak-update.service" ];
+    timerConfig = {
+      OnCalendar = "*-*-* 15:20:00";
+      Unit = "flatpak-update.service";
+    };
+  };
+  # XDG portals for sandboxed apps to work:
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+
+  services.spice-vdagentd.enable = true;
+
+  ### Podman ###
+  virtualisation = {
+    podman = {
+      enable = true;
+      # docker alias for podman
+      dockerCompat = true;
+    };
+  };
+
+
+
 
   system.copySystemConfiguration = true;
 
---
+++ nixos/packages.nix
@@ -0,0 +1,87 @@
+# This is the packages.nix file
+{ pkgs, ... }: with pkgs;
+#let
+#  nix-software-center = (import (pkgs.fetchFromGitHub {
+#    owner = "vlinkz";
+#    repo = "nix-software-center";
+#    rev = "0.1.0";
+#    sha256 = "d4LAIaiCU91LAXfgPCWOUr2JBkHj6n0JQ25EqRIBtBM=";
+#  })) {};
+#in 
+rec {
+
+  my-package-set = builtins.concatLists [
+      testing
+      cli
+      utils
+      gui
+      misc
+      kde
+    ];
+
+
+  testing = [
+      # nix-software-center
+      # ruby
+      trickle # network bandwidth limiter
+      keyd # Key remapping daemon
+      # youtube-dl
+      # yt-dlp
+      # obsidian
+      # brave
+      #sl
+      #pkg-config
+    ];
+  
+  cli = [
+      direnv
+      pv # progress viewer
+      desktop-file-utils
+      gettext
+      wget
+      git
+      # appimage-run
+      trash-cli
+      neofetch
+      firejail
+      efibootmgr
+      zsh
+      # distrobox
+      xorg.xhost
+      #spice-vdagent
+      x11spice
+    ];
+      
+  utils = [
+      nix-prefetch-docker # Used to get hash info for building docker images with nix
+      unstable.lynis # Security auditing tool
+    ];
+
+  gui = [
+      gparted
+      glxinfo #=glxgears
+      filelight 
+      firefox
+      librewolf
+      # ungoogled-chromium
+      # logseq
+      #signal-desktop
+      #latte-dock
+    ];
+
+  misc = [
+      gnome-icon-theme
+      #nuspell
+      hunspell
+      hunspellDicts.en-gb-ize
+    ];
+
+  kde = [
+      discover
+      # libsForQt5.kinfocenter # Info Centre
+      libsForQt5.kdialog # QT Dialog boxes for shell scripts
+      kate
+  ];
+
+}
+
