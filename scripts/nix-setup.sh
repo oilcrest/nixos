e6140ca03ddd11d8ae467da1e45161680b0511ee
John Garcia <jgarcia3788@yahoo.co.uk>
2023-04-02 19:19:47 +0100

    fixed nix run command

--- scripts/nix-setup.sh
+++ scripts/nix-setup.sh
@@ -97,7 +97,9 @@ function build_file_system {
     echo "WARNING - About to erase $DISK and install NixOS."
     prompt
 
-    nix --extra-experimental-features nix-command --extra-experimental-features flakes run github:nix-community/disko -- --mode zap_create_mount "../etc/nixos/disko-config.nix" --arg disks '[ "$DISK" ]'
+    nix --extra-experimental-features nix-command --extra-experimental-features flakes run github:nix-community/disko -- --mode zap_create_mount /root/nixos-main/etc/nixos/disko-config.nix --arg disks '[ "$DISK" ]'
+
+    # nix --extra-experimental-features nix-command --extra-experimental-features flakes run github:nix-community/disko -- --mode zap_create_mount /root/nixos-main/etc/nixos/disko-config.nix --arg disks '[ "/dev/vda" ]'
 
 
     # parted "$DISK" -- mklabel gpt
