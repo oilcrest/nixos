c58649cd5925103e90a9f5bec564dd8b50ba9d7a
John Garcia <jgarcia3788@yahoo.co.uk>
2023-04-04 14:43:19 +0100

    snapshot empty root

--- scripts/nix-setup.sh
+++ scripts/nix-setup.sh
@@ -89,8 +89,10 @@ function format_disko {
     # DISKO_CMD="nix --extra-experimental-features 'nix-command flakes' run github:nix-community/disko -- --mode zap_create_mount /root/nixos-main/etc/nixos/disko-config.nix --arg disks '[ ""\"""$DISK""\""" ]'"
     eval "$DISKO_CMD"
     # nix --extra-experimental-features nix-command --extra-experimental-features flakes run github:nix-community/disko -- --mode zap_create_mount /root/nixos-main/etc/nixos/disko-config.nix --arg disks '[ "/dev/vda" ]'
+    echo "Manually mounting disk"
     mount "$DISK"3 /mnt
-
+    echo "Making empty snapshot of root"
+    btrfs subvolume snapshot -r /mnt/ /mnt/rootfs-blank
 }
 
 function format_manual {
