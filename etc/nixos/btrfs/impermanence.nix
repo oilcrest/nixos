2e3654a0d84085a235b54e772225444553a7e8cb
John Garcia <jgarcia3788@yahoo.co.uk>
2023-04-05 17:34:53 +0100

    removed deletion of subvolumes - not needed

--- etc/nixos/btrfs/impermanence.nix
+++ etc/nixos/btrfs/impermanence.nix
@@ -45,12 +45,12 @@ in
     # in any issues so far, except for fairly
     # benign-looking errors from systemd-tmpfiles.
     
-    btrfs subvolume list -o /mnt/root |
-    cut -f9 -d' ' |
-    while read subvolume; do
-      echo "deleting /$subvolume subvolume..."
-      btrfs subvolume delete "/mnt/$subvolume"
-    done &&
+    # btrfs subvolume list -o /mnt/root |
+    # cut -f9 -d' ' |
+    # while read subvolume; do
+    #   echo "deleting /$subvolume subvolume..."
+    #   btrfs subvolume delete "/mnt/$subvolume"
+    # done &&
     echo "deleting root subvolume..." &&
     btrfs subvolume delete /mnt/root
 
