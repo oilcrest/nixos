159ee411b15bc45e5a7e57ee71d4d6840508820d
John Garcia <jgarcia3788@yahoo.co.uk>
2023-04-06 12:50:42 +0100

    test of dirs

--- etc/nixos/btrfs/disko-config.nix
+++ etc/nixos/btrfs/disko-config.nix
@@ -75,6 +75,9 @@
                   #   mountpoint = "/tmp";
                   #   mountOptions = [ "compress=zstd" "noatime" ];
                   # };
+                  nodev = {
+                    "/srv2" = { };
+                  };
                   "@/machines" = {
                     mountpoint = "/var/lib/machines";
                     mountOptions = [ "compress=zstd" "noatime" ];
