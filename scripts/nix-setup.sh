1f3e526e04345a5afca4177168800234326896ea
chewblacka <jgarcia3788@yahoo.co.uk>
2023-05-29 17:33:01 +0100

    Added trash folder to /persist

--- scripts/nix-setup.sh
+++ scripts/nix-setup.sh
@@ -199,7 +199,6 @@ function generate_config {
     mkdir -p /mnt/persist/etc/nixos
     cp -r /mnt/etc/nixos/* /mnt/persist/etc/nixos/
 
-
     echo "Copying over script files"
     mkdir -p /mnt/persist/scripts
     cp "$SCRIPTDIR"/* /mnt/persist/scripts
@@ -207,6 +206,11 @@ function generate_config {
     echo "Creating persist git path"
     mkdir -p /mnt/persist/git
     sudo chown 1000:users /mnt/persist/git
+    
+    echo "Creating trash folder for user 1000 in /persist"
+    mkdir -p /mnt/persist/.Trash-1000
+    sudo chown 1000:users /mnt/persist/.Trash-1000
+    sudo chmod 700 /mnt/persist/.Trash-1000
 
     # Write the password we entered earlier
     mkdir -p /mnt/persist/passwords
