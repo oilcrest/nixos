9628e55a3c3c5cc36c0c2e9bf17e5fad4c843aa1
chewblacka <jgarcia3788@yahoo.co.uk>
2023-05-31 17:40:14 +0100

    Update myparams.nix

--- etc/nixos/myparams.nix
+++ etc/nixos/myparams.nix
@@ -1,14 +1,11 @@
-# Username,
-# Password
-# Hostname
+# myparams.nix
 
 { config, lib, ... }:
 with lib;
-with types;
 {
   options = {
     myParams = mkOption {
-      type = attrs; # Should probably be `submodule?
+      type = types.attrs; # Should probably be `submodule?
       description = "My config attrs";
     };
   };
@@ -17,6 +14,7 @@ with types;
       myusername = "nixuser";
       myhostname = "nixos";
       mysshkey = "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBBDj2FdHqTEFS2AFwVXbc/93v+tKlD5MlSOFwWlGAJoNVFuOZh0sptdnaDR1XwIFCfGtFGvx0vNHJxe8uIFUbP0= (none)";
+      mydesktop = "kde";
     };
   };
 }
